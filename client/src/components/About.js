b0833a8b92dbf7536233e04a0dbeaa9d7fd8aaac
KrysieDee <krystal.smithx@gmail.com>
2018-11-18 17:36:31 -0500

    resolving conflicts

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Footer from './footer';
 
 
-export default () => {
-	return <div id="maincontainer">
-	<div id="about">
-				<div>
-					We like craft breweries and we particularly like those which allow
+import { Container, Divider } from 'semantic-ui-react'
+
+const ContainerExampleAlignment = () => (
+  <div>
+    <Container textAlign='left'></Container>
+    <Container textAlign='center'>About Us</Container>
+    <Container textAlign='right'></Container>
+    <Container textAlign='justified'>
+      <Divider />
+      <p>
+	        We like craft breweries and we particularly like those which allow
 					dogs, are open during the day, and have WiFi so we can code away
 					with our best buddies. While there are other websites and
 					resources available, none had an easy-to-use,
@@ -14,15 +20,14 @@ export default () => {
 					up-to-date, curated, and publically available database of
 					breweries for the betterment of the beer community and joy of web
 					developers like us.
-          		</div>
-	
-	
-	<div id='footer' >
+      </p>
+    </Container>
+
+		<div id='footer' >
 		<Footer />
 	</div>
-</div>
-</div>
-}
 
+  </div>
+)
 
-
+export default ContainerExampleAlignment
