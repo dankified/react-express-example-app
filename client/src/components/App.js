23124bbbcf1e263eaa936163755468b00850f0b7
leopolicastro <lpolicastro84@gmail.com>
2018-11-19 15:20:33 -0500

    Scroll on submit functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
 		axios.get(`https://api.openbrewerydb.org/breweries?by_city=${city}`)
 			.then((res) => {
 				let data = res.data;
-				this.setState({ breweriesList: data, hasList: true })
+				this.setState({ breweriesList: data, hasList: true }, document.getElementById('button').scrollIntoView())
 				console.log(data)
 			})
 	}
@@ -59,7 +59,9 @@ class App extends React.Component {
 				</div>
         <div>
           <Slideshow id='slideshow' />
-          {this.state.hasList === false ? null : <BreweriesList breweriesList={this.state.breweriesList} />}
+				<div id="button">
+						{this.state.hasList === false ? null : <BreweriesList breweriesList={this.state.breweriesList} />}
+					</div>
         </div>
         <div id='footer' >
           <Footer />
